7208ecc121933b8ba1759000898a84fada6a6a2b
Ibrahim <adeniyiib360@gmail.com>
2021-03-28 00:30:58 +0100

    feat: add static page generated with ssg

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import Head from 'next/head';
 import Link from 'next/link';
-import { fetchPosts } from '../redux/actions';
 import { useSelector } from 'react-redux';
 import { wrapper } from '../redux/store';
 import styles from '../styles/Home.module.css';
@@ -16,26 +15,6 @@ export const getServerSideProps = wrapper.getServerSideProps(
       payload: posts,
     });
     // store.dispatch(fetchPosts());
-    // store.dispatch({
-    //   type: 'SET_POSTS',
-    //   payload: [
-    //     {
-    //       userId: 1,
-    //       id: 1,
-    //       title:
-    //         'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-    //       body:
-    //         'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-    //     },
-    //     {
-    //       userId: 1,
-    //       id: 2,
-    //       title: 'qui est esse',
-    //       body:
-    //         'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
-    //     },
-    //   ],
-    // });
   }
 );
 
